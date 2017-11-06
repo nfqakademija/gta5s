8d3bf8351c9e9d6285eef04a14e6b7b01a610e3e
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-11-07 00:02:08 +0200

    Removed username field from Registration form.

---
+++ src/AppBundle/Form/RegistrationFormType.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kkraujelis
+ * Date: 11/6/17
+ * Time: 11:53 PM
+ */
+
+namespace AppBundle\Form;
+
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseFormType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationFormType extends BaseFormType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+        $builder->remove("username");
+    }
+
+}
