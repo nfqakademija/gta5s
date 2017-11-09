74d89437247de5af6b449aa92c355ecd46d73c76
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-11-09 19:54:04 +0200

    Updated requirements tab with newest information.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,18 +4,22 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/dashboard/", name="dashboard")
+     */
+    public function dashboardAction()
+    {
+        return $this->render('default/dashboard.html.twig', []);
     }
 }
