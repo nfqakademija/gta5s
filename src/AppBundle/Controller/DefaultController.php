56e3b1b1f63f377fb88fed889880dda6e32579c9
vladasko <vladaskoncius@gmail.com>
2017-11-14 13:36:44 +0200

    defaultcontroller added dashboard action

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,11 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("dashboard", name="dashboard")
+     */
+    public function dashboardAction() {
+        return $this->render('default/dashboard.html.twig');
+    }
 }
