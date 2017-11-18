d8baa9f1ce6d479bdb1f117643268633896b23f6
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-11-18 17:36:29 +0200

    Implemented Route To fetch information of player coords.

--- src/AppBundle/Controller/MapController.php
+++ src/AppBundle/Controller/MapController.php
@@ -9,14 +9,38 @@
 namespace AppBundle\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MapController extends Controller
 {
+
+    /**
+     * @Route("/json/map", name="json_map")
+     */
     public function getPlayersAction()
     {
         $em = $this->getDoctrine()->getManager();
 
-        $ = $em->getRepository('AppBundle:History')->findAll();
+        $actions = $em->getRepository('AppBundle:History')
+            ->getNewestHistoryActionByOnlinePlayer();
+
+        $map_data = [];
+        $map_data['players'] = [];
+
+        foreach($actions as $action) {
+
+            $account = $action->getAccount();
+            $details = json_decode($action->getDetails());
+
+            $obj = [];
+            //$obj['firstName'] = $account->getFirstName();
+            //$obj['lastName'] = $account->getLastName();
+            $obj['position'] = $details->{'pos'};
+            $map_data['players'][$account->getId()] = $obj;
+
+        }
+
+        return $this->json($map_data);
     }
 }
