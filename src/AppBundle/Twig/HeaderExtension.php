670042b300f9b0a0c01039df7ee383842562be5a
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-11-19 17:57:30 +0200

    Forgot to commit HeaderExtension

---
+++ src/AppBundle/Twig/HeaderExtension.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kkraujelis
+ * Date: 11/19/17
+ * Time: 5:26 PM
+ */
+
+namespace AppBundle\Twig;
+
+use AppBundle\Service\MasterListReader;
+use AppBundle\Service\MasterListServerInfo;
+use AppBundle\Service\ServerOfflineException;
+
+class HeaderExtension extends \Twig_Extension
+{
+
+    private $currData;
+
+    public function __construct(MasterListReader $masterListReader)
+    {
+        try {
+            $this->currData = $masterListReader->fetch();
+        }
+
+        catch (ServerOfflineException $e) {
+            $this->currData = null;
+        }
+    }
+
+    public function getFunctions() : array
+    {
+        return array(
+          new \Twig_SimpleFunction('server_data', array($this, 'serverData'))
+        );
+    }
+
+    public function serverData() : ?MasterListServerInfo
+    {
+
+        return $this->currData;
+
+    }
+
+
+}
