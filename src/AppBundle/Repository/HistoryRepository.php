263a488917712b92f756c6a5f5a87b9d027bbd27
root <root@vps399722.ovh.net>
2017-11-23 22:40:53 +0100

    I hate you for leaving this haha

--- src/AppBundle/Repository/HistoryRepository.php
+++ src/AppBundle/Repository/HistoryRepository.php
@@ -20,7 +20,7 @@ class HistoryRepository extends EntityRepository
      *
      * @return array
      */
-    public function getNewestHistoryActionByOnlinePlayer(string $datetime = "2017-11-18 14:55:46") : array
+    public function getNewestHistoryActionByOnlinePlayer(string $datetime = "now") : array
     {
 
         $to = new \DateTime($datetime);
