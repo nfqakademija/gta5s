79fbca923b1e7500250eb57a614c36d746141eb0
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-12-02 18:28:02 +0200

    Forgot to attach to last commit

---
+++ src/AppBundle/Form/ProfileFormType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Account;
+use FOS\UserBundle\Form\Type\ProfileFormType as BaseFormType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProfileFormType extends BaseFormType
+{
+
+    public function __construct()
+    {
+        parent::__construct(Account::class);
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder->remove('username');
+        $builder->remove('email');
+        $builder->add('bio');
+    }
+}
