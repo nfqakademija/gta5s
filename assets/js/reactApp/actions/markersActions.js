6619a12f593e6c9519e508b487aeee7beacfbf82
vladasko <vladaskoncius@gmail.com>
2017-12-05 21:30:59 +0200

    react and package json 0.3 thun, preparation for oop map

--- assets/js/reactApp/actions/markersActions.js
+++ assets/js/reactApp/actions/markersActions.js
@@ -1,5 +1,10 @@
 import * as types from "./actionTypes";
+const siteUrl = window.location.href;
 
-export function loadMarkers() {
-    return {type: types.LOAD_MARKERS_SUCCESS, markers: [{yea: 'yeah!'}]};
+export function loadMarkersJson(dispatch) {
+    return function(dispatch) {
+    return fetch(siteUrl + "json/map")
+        .then(response => response.json())
+        .then(json => dispatch({type: types.LOAD_MARKERS_SUCCESS, markers: json}));
+    }
 }
