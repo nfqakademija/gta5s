968d8e736041aa57e45d791c5730b4e7e573ba0c
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-12-07 09:55:19 +0200

    Added and configured OneUpUploaderBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new \Oneup\UploaderBundle\OneupUploaderBundle(),
             new AppBundle\AppBundle(),
         ];
 
