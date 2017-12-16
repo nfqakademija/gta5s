2906d5e9f39f50e4121910f454df28b5caac4b16
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-12-17 00:32:11 +0200

    Tops now show pages of 20 entries instead of 2

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -39,7 +39,7 @@ class DefaultController extends Controller
         $pagination = $paginator->paginate(
             $query, /* query NOT result */
             $request->query->getInt('page', 1)/*page number*/,
-            2/*limit per page*/
+            20/*limit per page*/
         );
 
         return $this->render('default/topai.html.twig', array('pagination' => $pagination));
