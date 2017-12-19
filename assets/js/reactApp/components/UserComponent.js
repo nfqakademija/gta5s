dd94f8300fd2ca1f30c2a06c03e2dc589fce61d1
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-12-19 15:54:15 +0200

    Removed unused button.

--- assets/js/reactApp/components/UserComponent.js
+++ assets/js/reactApp/components/UserComponent.js
@@ -9,9 +9,6 @@ const UserComponent = ({activeUser, closeUser, userEventsDropdown}) =>
                 <p><i className="fa fa-user-circle" aria-hidden="true"></i> {activeUser.user}</p>
                 <a href={"/profile/" + activeUser.user_id}>Profilis</a>
                 <span onClick={closeUser} className="map-user-x">&#10005;</span>
-                <button onClick={userEventsDropdown} id="timer-button" class="btn btn-primary user-events-dropdown-button">
-                    ką veikiu <span class="caret"></span>
-                </button>
             </div>
             <div class="user-events-menu user-events-hidden">
                 <ul>
